fbc71936945b4758f62bcc92b6cda64a817a81f3
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-04-08 05:21:58 +0000

    basics of java

---
+++ java/01_basics/hello.java
@@ -0,0 +1,11 @@
+// Hello word program in java 
+public class hello {
+    public static void main(String args[]) {
+        // System.out.println("Hello Java");
+        // program to add 2 numbers in java
+
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
+    }
+}
