b482b8d7953be11340dc518e2b901381a5b89f74
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-04-09 11:30:02 +0000

    variables & pattern

---
+++ java/01_basics/variables.java
@@ -0,0 +1,18 @@
+public class variables {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 30;
+        String name = "Larry fink";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+
+        String jaadoo = "Elon musk";
+        System.out.println(jaadoo);
+
+        // we can change the value of variable
+
+        b = 56;
+        System.out.println(b);
+    }
+}
