2e7f207a41f8d855dae780dc104f649690a7ce19
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-04-10 08:36:57 +0000

    data types in java

---
+++ java/01_basics/d_type.java
@@ -0,0 +1,17 @@
+public class d_type {
+    public static void main(String args[]) {
+        byte b = 20;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean var = true;
+        System.out.println(var);
+        double f1 = 10.52; 
+        System.out.println(f1);
+        float price = 10.5;
+        //long
+       
+        short n = 240;
+        System.out.println(n);
+    }
+}
