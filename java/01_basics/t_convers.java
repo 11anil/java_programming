db49bf8d645d1fcfd92f0435d391792dd3fa4231
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-04-12 16:10:37 +0000

    type-conversion-in-java

---
+++ java/01_basics/t_convers.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+public class t_convers {
+    public static void main(String args[]){
+      
+    //    int a= 25;
+    //    float b = a;
+
+    // float a = 25;  
+      //  int b = a;
+   
+      //this will throw an error that conversion of float to int is not possible because float store 8 bytes and int store 4 bytes
+//int to float is compatible but float to int is not compatible
+
+//type-conversion:
+//byte->short->int->float->long->double->
+
+    //    System.out.println(b);
+      
+
+
+       Scanner sc = new Scanner(System.in);
+    //    int number = sc.nextFloat();
+    //    System.out.println(number);
+   float number = sc.nextInt();
+    System.out.println(number);
+
+       sc.close();
+}
+}
