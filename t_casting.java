26046c9d5e91680925d7ab41f646e5f0357fa594
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-04-13 16:22:39 +0000

    type-casting-in-java

---
+++ t_casting.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class t_casting {
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    // float marks = 99.9999f;
+    // int marks2 = (int) 99;
+    // System.out.println(marks2);
+
+
+    // charcter to number(int) conversion
+
+    Character ch = 'a';
+    int number = ch;
+    System.out.println(number);
+    Character ch2 = 'b';
+    int number2 = ch2;
+    System.out.println(number2);
+    sc.close();
+  }  
+}
