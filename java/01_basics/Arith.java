0daa9e979b0f5f197b74322ce8edf1308a004126
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-04-16 14:22:57 +0000

    Arithmetic operators in java

---
+++ java/01_basics/Arith.java
@@ -0,0 +1,16 @@
+// import java.util.*;
+public class Arith {
+    public static void main(String args[]) {
+
+        //Arithmetic operators like basic simple operators ex-Add.sub,div,mul
+
+     int a = 8;
+     int b = 4;
+     System.out.println("Add = " + (a+b));
+     System.out.println("dii = " + (a-b));
+     System.out.println("mul = " + (a*b));
+     System.out.println("div = " + (a/b));
+     System.out.println("mod = " + (a%b));
+    }
+
+}
