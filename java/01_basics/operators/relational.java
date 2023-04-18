9b8d7760ad76479b1e328d9448e851d8253408b2
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-04-18 08:25:28 +0000

    relational operators in java

---
+++ java/01_basics/operators/relational.java
@@ -0,0 +1,37 @@
+package operators;
+
+public class relational {
+    public static void main(String args[]){
+
+       // relational operators
+        
+       int  a = 10;
+       int  b = 5;
+    
+     //equal to
+
+      System.out.println(a==b);
+
+      // not equal to
+
+      System.out.println(a!=b);
+      
+      // greator than
+
+      System.out.println(a>b);
+
+      //less than
+
+      System.out.println(a<b);
+
+      //greator than - equal to
+
+      System.out.println(a>=b);
+
+
+    //   less than - equal to
+
+      System.out.println(a<=b);
+
+    }
+}
