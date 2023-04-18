4d128182edc398233cf311a5afef5f4613427604
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-04-18 08:05:25 +0000

    unary opeartors in java

---
+++ java/01_basics/operators/unary.java
@@ -0,0 +1,32 @@
+package operators;
+
+public class unary {
+    public static void main (String args[]){
+
+        //pre increment 
+        // 1-value change 
+        //then value use
+
+        // so result will be 11 and 11
+
+        // int a = 10;
+        // int b = ++a;
+        // System.out.println(a);
+        // System.out.println(b);
+
+
+         //post increment 
+        // 1-value use
+        //then value change
+
+        // so result will be 11 and 10
+
+        int a = 10;
+        int b = a++;
+        System.out.println(a);
+        System.out.println(b);
+
+
+        //similar for decrement also
+    }
+}
