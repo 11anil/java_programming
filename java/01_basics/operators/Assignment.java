424cde921b795d721ab3f1578c25b1a44049df3a
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-04-18 09:04:15 +0000

    Assignment operators in java

---
+++ java/01_basics/operators/Assignment.java
@@ -0,0 +1,28 @@
+package operators;
+
+public class Assignment {
+    public static void main(String args[]) {
+        // Assignment operators
+
+        int a = 5;
+        // // += operator
+        // a += 1;
+        // System.out.println(a);
+
+        // -= operator
+        // a -= 1;
+        // System.out.println(a);
+
+        // *= operator
+        // a *= 1;
+        // System.out.println(a);
+
+        // /// = operator
+        // a /= 1;
+        // System.out.println(a);
+
+        // %= operator
+        a %= 1;
+        System.out.println(a);
+    }
+}
