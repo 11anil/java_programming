f99fc2bd292437786c9ccb27842b210706d32d96
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-04-20 18:17:02 +0000

    if-else in java

---
+++ java/01_basics/operators/conditional_operators/if_else.java
@@ -0,0 +1,14 @@
+package operators.conditional_operators;
+
+public class if_else {
+    public static void main(String args[]){
+    int age = 16 ;
+    if(age>18){
+        System.out.println("adult: vote,drive");
+    }
+    else{
+        System.out.println("not adult");
+    
+    }
+}
+}
