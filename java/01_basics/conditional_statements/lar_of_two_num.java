ccf82ef1ebdfb8dc50843d24d4f63d6cf651d193
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-04-22 13:38:54 +0000

    largest of two numbers

---
+++ java/01_basics/conditional_statements/lar_of_two_num.java
@@ -0,0 +1,15 @@
+package conditional_statements;
+
+public class lar_of_two_num {
+    public static void main(String args[]) {
+        int a = 2;
+        int b = 1;
+    
+      if(a>=b){
+        System.out.println("a is largest");
+      }  
+      else{
+        System.out.println("b is largest");
+      }
+    }
+}
