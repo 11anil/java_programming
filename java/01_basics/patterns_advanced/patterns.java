18054a42c2562dc87f2d8953ddb7a8bd67f67447
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-04-26 06:47:11 +0000

    A litttle change in location

---
+++ java/01_basics/patterns_advanced/patterns.java
@@ -0,0 +1,20 @@
+package patterns_advanced;
+public class patterns {
+    public static void hollow_rectangle(int totrows, int totcol) {
+        for (int i = 1; i <= totrows; i++) {
+            for (int j = 1; j <= totcol; j++) {
+                if (i == 1 || i == totrows || j == 1 || j == totcol) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+public static void main(String args[]){
+   hollow_rectangle(4,5);
+}
+}
+
