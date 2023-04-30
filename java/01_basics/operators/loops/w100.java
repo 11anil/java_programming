9df7328b546f73c4c5c88ccab72e1167299f38a1
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-04-30 08:21:33 +0000

    while loop in java

---
+++ java/01_basics/operators/loops/w100.java
@@ -0,0 +1,12 @@
+package operators.loops;
+
+public class w100 {
+    public static void main(String args[]){
+        int counter = 0;
+        while(counter<10){
+            System.out.println("ham honge kamiyab");
+            counter++;
+        }
+        System.out.println("printed ham honge kamiyab 10x");
+    }
+}
