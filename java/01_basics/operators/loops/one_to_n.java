d6ab6eab7d68c4e218ac52719481744fe98ba3f2
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-04-30 08:42:15 +0000

    print num  from one to n

---
+++ java/01_basics/operators/loops/one_to_n.java
@@ -0,0 +1,18 @@
+package operators.loops;
+
+import java.util.*;
+
+public class one_to_n {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int range = sc.nextInt();
+        int counter = 1;
+        while(counter<=range){
+            System.out.print(counter + " ");
+            counter++;
+        }
+        System.out.println();
+        sc.close();
+    }
+   
+}
