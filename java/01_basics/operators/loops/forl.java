3b426d30c3271f2f7859d20cdc2f5f2178918776
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-01 06:02:09 +0000

    for loop in java

---
+++ java/01_basics/operators/loops/forl.java
@@ -0,0 +1,9 @@
+package operators.loops;
+
+public class forl {
+    public static void main (String args[]){
+        for(int i =1; i<=10; i++){
+            System.out.println("Ham honge kamiyab");
+        }
+    }
+}
