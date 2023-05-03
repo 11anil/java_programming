c9be53146820e6c3071bde73f418922b9ace83d1
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-03 14:36:12 +0000

    do while loop in java

---
+++ java/01_basics/loops/dwhil.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class dwhil {
+    public static void main(String args[]){
+       
+        int counter = 1;
+       
+        do{
+            System.out.println("Ham Honge kamiyab");
+            counter++;
+        }while(counter<=10);
+    }
+}
