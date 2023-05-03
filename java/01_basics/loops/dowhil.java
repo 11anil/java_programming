c892b212fb36d7190e2ba316c76ca0ee42511c9a
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-03 14:47:41 +0000

    do while loop in java

--- java/01_basics/loops/dowhil.java
+++ java/01_basics/loops/dowhil.java
@@ -2,14 +2,10 @@ package loops;
 
 public class dowhil {
     public static void main(String args[]){
-       
         int counter = 1;
-       
         do{
-            System.out.println("Ham Honge kamiyab");
+            System.out.println("Ham Honge Kamiyab");
             counter++;
         }while(counter<=10);
     }
 }
-
-
