d74b808330feff05a1875b9e28be7a0681be5e0e
Mohit Singh <123624325+Intruder9211@users.noreply.github.com>
2023-05-03 19:05:14 +0530

    Update reve.java

--- java/01_basics/loops/reve.java
+++ java/01_basics/loops/reve.java
@@ -4,7 +4,7 @@ public class reve {
     public static void main(String args[]) {
         int n = 23546;
         while(n>0){
-           int lastdigit = n%10;
+            int lastdigit = n%10;
             System.out.print(lastdigit + " ");
             n = n/10;
         }
