b0e167b9a95993075a278fdba407fc57395ccd28
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-03 14:42:36 +0000

    do while loop

---
+++ java/01_basics/loops/dowhil.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class dowhil {
+    public static void main(String args[]){
+       
+        int counter = 1;
+       
+        do{
+            System.out.println("Ham Honge kamiyab");
+            counter++;
+        }while(counter<=10);
+    }
+}
+
+
