fe5e23d95b6bd0dbcaad1e0958d2d1844bacc1d7
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-10 14:53:16 +0000

    continue keyword question

--- java/01_basics/loops/continu.java
+++ java/01_basics/loops/continu.java
@@ -1,10 +1,37 @@
-public class continu {
-    public static void main(String args[]){
-        for(int i =1; i<=5; i++){
-            if(i==3){
-               continue;
-            }
-            System.out.println(i);
-        }
+// import java.util.*;
+
+// public class continu {
+
+// continue statement
+
+// public static void main(String args[]){
+// for(int i =1; i<=5; i++){
+// if(i==3){
+// continue;
+// }
+// System.out.println(i);
+// }
+// }
+// }
+
+// Q- continue statement
+
+import java.util.Scanner;
+
+class Continu {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.print("Enter your number : ");
+            int n = sc.nextInt();
+            if (n % 10 == 0) {
+                continue;
+            } 
+            System.out.println("Number entered: " + n);
+            // sc.close(); 
+        } while (true);
+         
     }
 }
+
