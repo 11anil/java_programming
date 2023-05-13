4ab3a1d06f75a0666ee32f46dbc0f093c0e52842
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-13 09:36:02 +0000

    functions intro in java

---
+++ .vscode/functions/hw.java
@@ -0,0 +1,8 @@
+public class hw{
+    public static void printhelloworld(){
+        System.out.println("Hello World");
+    }
+    public static void main(String args[]){
+        printhelloworld();
+    }
+}
