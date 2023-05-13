b33ff6d814f3133ef88af73cdc840900b3acccea
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-13 09:48:13 +0000

    Functions in java

---
+++ java/functions/hw.java
@@ -0,0 +1,8 @@
+public class hw{
+    public static void printhelloworld(){
+        System.out.println("Hello Duniya");
+    }
+    public static void main(String args[]){
+        printhelloworld();
+    }
+}
