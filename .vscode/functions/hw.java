0caceaaac2bc071226535bac598e6ca86613c0e9
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-13 09:45:55 +0000

    functions intro

--- .vscode/functions/hw.java
+++ .vscode/functions/hw.java
@@ -1,6 +1,6 @@
 public class hw{
     public static void printhelloworld(){
-        System.out.println("Hello World");
+        System.out.println("Hello Duniya");
     }
     public static void main(String args[]){
         printhelloworld();
