83985001fcd2ada9acb39802a436981fa42e06fa
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-15 05:27:29 +0000

    function overloading using data-types

--- .vscode/functions/foverload.java
+++ .vscode/functions/foverload.java
@@ -1,15 +1,27 @@
 public class foverload{                          // function overloading -- same name but different parameters
 
-    //funct to calculate sum of 2 numbers
-    public static int sum(int a , int b){
-        return a+b;
+    // //funct to calculate sum of 2 numbers
+    // public static int sum(int a , int b){
+    //     return a+b;
+    // }
+    // //func to calculate sum of 3 numbers
+    // public static int sum(int a, int b , int c){
+    //     return a+b+c;
+    // }
+
+    // function to calculate int sum
+
+    public static int sum(int a, int b){
+        return a + b;
     }
-    //func to calculate sum of 3 numbers
-    public static int sum(int a, int b , int c){
-        return a+b+c;
+
+    // function to calculate float sum
+
+    public static float sum(float a , float b){
+        return  a + b;
     }
     public static void main(String args[]){
        System.out.println (sum(3,5));
-       System.out.println (sum(3,5,1));
+       System.out.println (sum(3.2f,5.8f));
     }
 }
