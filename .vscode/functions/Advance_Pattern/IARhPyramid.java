d34cb770a25658f0bb1f315162dd653fb2b4388e
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-23 02:25:15 +0000

    inverted half pyramid with numbers

--- .vscode/functions/Advance_Pattern/IARhPyramid.java
+++ .vscode/functions/Advance_Pattern/IARhPyramid.java
@@ -14,7 +14,19 @@ public class IARhPyramid {
             System.out.println();
         }
     }
+
+    public static void inverted_half_pyramid_with_num(int n){
+        //outer loop
+        for(int i=1; i<=n; i++){
+            // outer loop
+            for(int j=1; j<=n-i+1; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String args[]){
-        inverted_rotated_half_pyramid(4);
+        // inverted_rotated_half_pyramid(4);
+        inverted_half_pyramid_with_num(5);
     }
 }
