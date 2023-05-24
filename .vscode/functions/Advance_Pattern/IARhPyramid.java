80bbb31a169c84fe927950114e8137d43391145c
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-24 10:29:05 +0000

    Zero one triangle

--- .vscode/functions/Advance_Pattern/IARhPyramid.java
+++ .vscode/functions/Advance_Pattern/IARhPyramid.java
@@ -39,9 +39,23 @@ public class IARhPyramid {
             System.out.println();
         }
     }
+    public static void zero_one_triangle(int n){
+        for(int i=1; i<=n; i++){
+          for(int j=1; j<=i; j++){
+            if((i+j)%2 == 0){
+              System.out.print("1");
+            }else{
+              System.out.print("0");
+            }
+          }
+          System.out.println();
+        }
+      }
+    
     public static void main(String args[]){
         // inverted_rotated_half_pyramid(4);
         // inverted_half_pyramid_with_num(5);
-        floyd_triangle(5 );
+        // floyd_triangle(5 );
+        zero_one_triangle(5);
     }
 }
