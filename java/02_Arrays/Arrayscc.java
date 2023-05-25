a33dd92f4d6347c6ad1f8da6fad5e7b79a5081b0
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-25 08:28:15 +0000

    input output in array

---
+++ java/02_Arrays/Arrayscc.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+
+public class Arrayscc {
+    public static void main(String args[]){
+      int marks[] = new int[50];
+      Scanner sc = new Scanner(System.in);
+
+      System.out.println("length of array = " + marks.length);
+
+    //   marks[0] = sc.nextInt();
+    //   marks[1] = sc.nextInt();
+    //   marks[2] = sc.nextInt();
+
+    //   System.out.println("Marks of phy = " + marks[0]);
+    //   System.out.println("Marks of chm = " + marks[1]);
+    //   System.out.println("Marks of math = " + marks[2]);
+
+    //   marks[2] = marks[2] + 1;
+    //   System.out.println("Marks of math = " + marks[2]);
+
+    //   int percentage = ((marks[0] + marks[1] + marks[2] )/ 3);
+    //   System.out.println("percentage = " + percentage + "%" ) ;
+    }
+}
+
+
