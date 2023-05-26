eea45ca8e4b752e45a3131500b2e38f71f91d7db
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-26 10:51:09 +0000

    String charAt method

--- java/05_Strings/slength.java
+++ java/05_Strings/slength.java
@@ -1,11 +1,20 @@
 public class slength {
+    public static void printletters(String str){
+        for(int i=0; i<str.length(); i++){
+            System.out.print(str.charAt(i) + " ");
+        }
+        System.out.println();
+    }
     public static void main(String args[]){
     //     String fullname = "Anil Kumar";
     //    System.out.println(fullname.length()) ;
     
      String firstname = "Anil";
-     String lastname = "Kumar";
-     String fullname = firstname + " " + lastname;
-     System.out.println(fullname);
+    //  String lastname = "Kumar";
+    //  String fullname = firstname + " " + lastname;
+    //  System.out.println(fullname);
+
+     printletters(firstname);
+
     }
 }
