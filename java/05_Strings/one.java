2235fe018b0be2521eb6eb3fd22fc3ba8198cc8e
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-26 10:33:15 +0000

    strings input/output

---
+++ java/05_Strings/one.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class one {
+    public static void main(String args[]){
+        char arr[] = {'a','b','c','d'};
+        String str = "avsjsdw98e7";
+        String str2 = new String("aabra ka dabra");
+
+        //Strings are immutable in java --
+
+        // means string co[annot be changed after it will cfreate a new string not replaced]
+        
+       Scanner in = new Scanner(System.in);
+       String name;
+    //    name = in.next(); // for a word
+         name = in.nextLine(); //for a sentence
+
+       System.out.println(name);
+    }
+}
