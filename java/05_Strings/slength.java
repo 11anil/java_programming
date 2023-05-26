286e43941f12b9f26ccea41e271af87c8aa6f25f
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-26 10:44:17 +0000

    String Concatenate

--- java/05_Strings/slength.java
+++ java/05_Strings/slength.java
@@ -1,6 +1,11 @@
 public class slength {
     public static void main(String args[]){
-        String fullname = "Anil Kumar";
-       System.out.println(fullname.length());
+    //     String fullname = "Anil Kumar";
+    //    System.out.println(fullname.length()) ;
+    
+     String firstname = "Anil";
+     String lastname = "Kumar";
+     String fullname = firstname + " " + lastname;
+     System.out.println(fullname);
     }
 }
