f07b319dc0c3db4eb0ea490b7230d25feae22a25
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-28 09:19:21 +0000

    Substring in java

--- java/05_Strings/equal.java
+++ java/05_Strings/equal.java
@@ -1,8 +1,15 @@
 public class equal {
+    public static String substring(String str,int si, int ei){
+     String substr = "";
+     for(int i=si; i<ei; i++){
+        substr += str.charAt(i);
+     }
+     return substr;
+    }
     public static void main(String args[]){
-        String s1 = "Tony";
-        String s2 = "Tony";
-        String s3 = new String("Tony");
+        // String s1 = "Tony";
+        // String s2 = "Tony";
+        // String s3 = new String("Tony");
 
         // if(s1==s2){
         //     System.out.println("Strings are equal");
@@ -15,11 +22,14 @@ public class equal {
         //     System.out.println("Strings are not equal");
         // }
 
-        if(s1.equals(s3)){
-            System.out.println("String are equal");
-        }else{
-            System.out.println("String are not equal");
-        }
+        // if(s1.equals(s3)){
+        //     System.out.println("String are equal");
+        // }else{
+        //     System.out.println("String are not equal");
+        // }
+
+        String str = "HELLOWORLD";
+        System.out.println(substring(str, 0, 5));
     }
 }
 //this method(.equals) is used when the value of strings are same
