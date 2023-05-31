b5e4505b216de5993c3909f01d67b14fbb601fa9
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-31 09:36:09 +0000

    Clear Ith Bit

--- java/06_Bit Manipulation/oddoreven.java
+++ java/06_Bit Manipulation/oddoreven.java
@@ -9,7 +9,8 @@ public class oddoreven {
             System.out.println("Odd Number");
         }
     }
-
+    
+          
     public static int getIthBit(int n, int i) {
         int bitMask = 1 << i;
         if ((n & bitMask) == 0) {
@@ -22,13 +23,18 @@ public class oddoreven {
         int bitMask = 1<<i;
         return n|bitMask;
        }
+       public static int clearIthBit(int n , int i){
+        int bitMask = ~(1<<i);
+        return n & bitMask;
+       }
     public static void main(String args[]) {
         // OddorEven(11);
         // OddorEven(13);
         // OddorEven(14);
 
         // System.out.println(getIthBit(10, 3));
-        System.out.println(setIthBit(10,2));
+        // System.out.println(setIthBit(10,2));
+        System.out.println(clearIthBit(10,1));
 
     }
 }
