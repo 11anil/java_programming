b909ed44bfd86751b965c11c9798f3e03b4deb69
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-31 09:24:35 +0000

    IthBit

--- java/06_Bit Manipulation/oddoreven.java
+++ java/06_Bit Manipulation/oddoreven.java
@@ -1,18 +1,30 @@
 import java.util.*;
+
 public class oddoreven {
     public static void OddorEven(int n) {
         int bitMask = 1;
-        if((n & bitMask) == 0) {
+        if ((n & bitMask) == 0) {
             System.out.println("Even Number");
         } else {
             System.out.println("Odd Number");
         }
     }
 
+    public static int getIthBit(int n, int i) {
+        int bitMask = 1 << i;
+        if ((n & bitMask) == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
     public static void main(String args[]) {
-        OddorEven(11);
-        OddorEven(13);
-        OddorEven(14);
+        // OddorEven(11);
+        // OddorEven(13);
+        // OddorEven(14);
+
+        System.out.println(getIthBit(10, 3));
 
     }
 }
