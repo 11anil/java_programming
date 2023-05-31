79bfc7ed92b3c504ece5bbf411fc9a2566d0a5e2
Anil Kumar <126347796+11anil@users.noreply.github.com>
2023-05-31 00:21:58 +0000

    Binary XOR

---
+++ java/06_Bit Manipulation/binXOR.java
@@ -0,0 +1,9 @@
+public class binXOR {
+    public static void main(String args[]){
+        System.out.println(5^6);  //XOR operation    // 5 - 101
+                                                     // 6 - 110
+                                                    //      ----
+                                                    //      011     // so the answer will be 3
+                                                    //      ----
+        }
+}
